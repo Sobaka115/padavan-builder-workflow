31f7c3c8b266671c0af9f0c7afb35c3aabf68f7e
Sobaka115 <40915601+Sobaka115@users.noreply.github.com>
2025-01-13 19:22:08 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,2 +1,2 @@
-# Отключение log wireguard debug
-sed -i 's/func_enable_kernel_param "CONFIG_WIREGUARD_DEBUG"/func_disable_kernel_param "CONFIG_WIREGUARD_DEBUG"/' padavan-ng/trunk/build_firmware.sh
+# Отключение log wireguard debug (перенес в post-build.sh)
+# sed -i 's/func_enable_kernel_param "CONFIG_WIREGUARD_DEBUG"/func_disable_kernel_param "CONFIG_WIREGUARD_DEBUG"/' padavan-ng/trunk/build_firmware.sh
